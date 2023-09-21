636c09377215de0823f4013e8b7d0a5ca7a6211d
Hemant <hemantvcp@gmail.com>
2023-09-21 16:32:11 +0530

    Theme added in separate clas

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:dart_5_days/pages/home_page.dart';
 import 'package:dart_5_days/pages/login_page.dart';
 import 'package:dart_5_days/utils/routes.dart';
+import 'package:dart_5_days/widget/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -15,9 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-          primarySwatch: Colors.deepPurple,
-          fontFamily: GoogleFonts.lato().fontFamily),
+      theme: AppThemeData.lightTheme(context),
       routes: {
         "/": (context) => HomePage(),
         MyRoutes.homeRoute: (context) => HomePage(),
